282ec18675098ec3e0e0cc0c33a2f9ef13648a96
Fulvio <Fulvio@192.168.1.45>
2013-03-06 14:39:44 +0100

    aaaaaa

--- CiaoMondo/src/com/webratio/bar/Ciao.java
+++ CiaoMondo/src/com/webratio/bar/Ciao.java
@@ -6,7 +6,7 @@ public class Ciao {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Ciao Mondo");
+		System.out.println("Ciao Mondo !");
 		// TODO Auto-generated method stub
 
 	}
