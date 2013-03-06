92a49c6eead3583bb639bbd44e87762e28a364cd
Fulvio <Fulvio@192.168.1.45>
2013-03-06 14:01:43 +0100

    Primo Commit

---
+++ CiaoMondo/src/com/webratio/bar/Ciao.java
@@ -0,0 +1,14 @@
+package com.webratio.bar;
+
+public class Ciao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Ciao Mondo");
+		// TODO Auto-generated method stub
+
+	}
+
+}
